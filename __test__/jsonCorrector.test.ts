041a4cfe7535f9918303cc5063733446a77b9709
hamshad <hiromega18@gmail.com>
2024-08-11 21:13:19 +0530

    chore: test updated

--- __test__/jsonCorrector.test.ts
+++ __test__/jsonCorrector.test.ts
@@ -5,4 +5,7 @@ test("jsonCorrector", () => {
   expect(jsonCorrector(data)).toBe(
     `{ "data": "hello", "name": "typescript's father isn't a javascript, instead I don't even care and you shouldn't too. If you do than you're time is being wasted" }`
   );
+  expect(jsonCorrector("{greet: 'good', greets: ['morning', 'afternoon', 'evening', 'night']}")).toBe(
+    `{"greet": "good", "greets": ["morning", "afternoon", "evening", "night"]}`
+  );
 });
